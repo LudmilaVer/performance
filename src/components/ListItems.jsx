0ad0b9271871ee18be38ab60b0e5f99e7c863ba5
LudmilaVer <gelmut1984@gmail.com>
2024-06-12 17:33:03 +0200

    Исправьте все выявленные проблемы, следуя рекомендациям ESLint.

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -33,6 +33,41 @@
 // export default ListItems;
 
 // добавление хука useEffect B ListItems "jsx"
+// import React, { useState, useEffect } from 'react';
+
+// function ListItems() {
+//     const [inputValue, setInputValue] = useState('');
+//     const [items, setItems] = useState([]);
+
+//     useEffect(() => {
+//         console.log('Component ListItems updated');
+//     }, [items]);
+
+//     const addItem = () => {
+//         setItems([...items, inputValue]);
+//         setInputValue('');
+//     };
+
+//     return (
+//         <div>
+//             <input
+//                 type="text"
+//                 value={inputValue}
+//                 onChange={(e) => setInputValue(e.target.value)}
+//             />
+//             <button onClick={addItem}>Add Item</button>
+//             <ul>
+//                 {items.map((item, index) => (
+//                     <li key={index}>{item}</li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
+
+// export default ListItems;
+
+
 import React, { useState, useEffect } from 'react';
 
 function ListItems() {
@@ -40,8 +75,8 @@ function ListItems() {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        console.log('Component ListItems updated');
-    }, [items]);
+        console.log('Component ListItems mounted');
+    }, []); // Пустой массив зависимостей гарантирует, что useEffect выполнится только один раз при монтировании компонента
 
     const addItem = () => {
         setItems([...items, inputValue]);
