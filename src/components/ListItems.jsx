eeabe37e9eef04e2c558caa55ff69b3ea60bf23c
LudmilaVer <gelmut1984@gmail.com>
2024-06-12 17:20:27 +0200

    Добавление намеренной ошибки в ListItems.js

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,33 +1,68 @@
+// import React, { useState, useEffect } from 'react';
+
+// function ListItems() {
+//   const [items, setItems] = useState([]);
+//   const [inputValue, setInputValue] = useState('');
+
+//   const addItem = () => {
+//     setItems([...items, inputValue]);
+//     setInputValue('');
+//   };
+
+//   useEffect(() => {
+//     console.log('Component ListItems updated');
+//   }, [items]);
+
+//   return (
+//     <div>
+//       <input 
+//         type="text" 
+//         value={inputValue} 
+//         onChange={(e) => setInputValue(e.target.value)} 
+//       />
+//       <button onClick={addItem}>Add Item</button>
+//       <ul>
+//         {items.map((item, index) => (
+//           <li key={index}>{item}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ListItems;
+
+// добавление хука useEffect B ListItems "jsx"
 import React, { useState, useEffect } from 'react';
 
 function ListItems() {
-  const [items, setItems] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [items, setItems] = useState([]);
 
-  const addItem = () => {
-    setItems([...items, inputValue]);
-    setInputValue('');
-  };
+    useEffect(() => {
+        console.log('Component ListItems updated');
+    }, [items]);
 
-  useEffect(() => {
-    console.log('Component ListItems updated');
-  }, [items]);
+    const addItem = () => {
+        setItems([...items, inputValue]);
+        setInputValue('');
+    };
 
-  return (
-    <div>
-      <input 
-        type="text" 
-        value={inputValue} 
-        onChange={(e) => setInputValue(e.target.value)} 
-      />
-      <button onClick={addItem}>Add Item</button>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={addItem}>Add Item</button>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default ListItems;
