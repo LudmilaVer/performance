db23e4adf1ca035277e0397ff34a56dc36a73c2c
LudmilaVer <gelmut1984@gmail.com>
2024-06-12 17:09:59 +0200

    Сохраните файл и убедитесь, что приложение запускается без ошибок и новый компонент отображается на странице.до ошибки

---
+++ src/components/ListItems.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+function ListItems() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addItem = () => {
+    setItems([...items, inputValue]);
+    setInputValue('');
+  };
+
+  useEffect(() => {
+    console.log('Component ListItems updated');
+  }, [items]);
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        value={inputValue} 
+        onChange={(e) => setInputValue(e.target.value)} 
+      />
+      <button onClick={addItem}>Add Item</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ListItems;
